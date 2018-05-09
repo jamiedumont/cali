a732861d96d6756cb0a005f5d36778d19c29879d
Jamie Dumont <jamiedumont@icloud.com>
2018-05-09 08:39:49 +0100

    Add social meta

--- lib/cali_web/views/post_view.ex
+++ lib/cali_web/views/post_view.ex
@@ -4,12 +4,24 @@ defmodule CaliWeb.PostView do
 
   def render("meta.show.html", assigns) do
     ~E{
+      <!-- Meta data -->
       <title><%= assigns.post.title %> - Jamie Dumont</title>
       <meta
         name="description"
         content="<%= assigns.post.description %>"
       >
       <link rel="canonical" href="<%= current_url(assigns.conn, %{\}) %>" />
+
+      <!-- Open Graph data -->
+      <meta property="og:title" content="<%= assigns.post.title %>" />
+      <meta property="og:url" content="<%= current_url(assigns.conn, %{\}) %>" />
+      <meta property="og:type" content="article" />
+      <meta property="og:description" content="<%= assigns.post.description %>" />
+
+      <!-- Twitter Card -->
+      <meta name="twitter:title" content="<%= assigns.post.title %>" />
+      <meta name="twitter:description" content="<%= assigns.post.description %>" />
+      <meta name="twitter:url" content="<%= current_url(assigns.conn, %{\}) %>" />
     }
   end
 end
