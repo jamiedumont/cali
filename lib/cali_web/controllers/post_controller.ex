1227d520d496d6f436e947fcb9423195bd08b857
Jamie Dumont <jamiedumont@icloud.com>
2018-05-14 12:10:17 +0100

    Set up automated deployment

--- lib/cali_web/controllers/post_controller.ex
+++ lib/cali_web/controllers/post_controller.ex
@@ -2,6 +2,7 @@ defmodule CaliWeb.PostController do
   use CaliWeb, :controller
 
   def show(conn, %{"slug" => slug}) do
+    IO.puts "Someone viewed #{slug}"
     case Cali.Repo.get_by_slug(slug) do
       {:ok, post} -> render conn, "show.html", post: post
       :not_found -> not_found(conn)
