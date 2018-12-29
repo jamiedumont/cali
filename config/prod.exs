48d83a22a178f8aa518f3580ec95048932be9181
Jamie Dumont <jamiedumont@icloud.com>
2018-12-29 15:28:54 +0000

    Update edeliver config

--- config/prod.exs
+++ config/prod.exs
@@ -5,8 +5,7 @@ config :cali, CaliWeb.Endpoint,
   url: [scheme: "https", host: "www.jamiedumont.co.uk", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
-  root: ".",
-  version: Application.spec(:cali, :vsn)
+  root: "."
 
 config :cali, CaliWeb.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
